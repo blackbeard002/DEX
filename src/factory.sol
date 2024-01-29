20b38c3ccf6afe041199bd8754d533095207d967
Deepu <deepuharry02@gmail.com>
2024-01-30 04:43:31 +0530

    Factory completed:NOT TESTED YET!

--- src/factory.sol
+++ src/factory.sol
@@ -1,20 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract Counter 
-{
-    address s_ExchangeTemplate;
-    mapping(address=>address) s_TokenToExchange;
-    mapping(address=>address) s_ExchangeToToken;
+import "./exchange.sol";
 
-    constructor(address template)
-    {
-        s_ExchangeTemplate=template; 
-    }
+contract Factory 
+{
+    mapping(address=>address) s_Token_To_Exchange;
+    mapping(address=>address) s_Exchange_To_Token;
 
     function createExchange(address token) public returns(address)
     {
-        require(s_TokenToExchange[token]==address(0));
+        require(s_Token_To_Exchange[token] == address(0));
         
+        address new_exchange = address(new Exchange());
+
+        s_Token_To_Exchange[token] = new_exchange;
+        s_Exchange_To_Token[new_exchange] = token; 
+
+        return new_exchange; 
     }
 }
