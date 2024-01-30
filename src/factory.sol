efa8fb646ea6020c980f2214776fc40f8bba43fb
Deepu <deepuharry02@gmail.com>
2024-01-30 22:13:54 +0530

    Factory completed and tested

--- src/factory.sol
+++ src/factory.sol
@@ -5,11 +5,14 @@ import "./exchange.sol";
 
 contract Factory 
 {
-    mapping(address=>address) s_Token_To_Exchange;
-    mapping(address=>address) s_Exchange_To_Token;
+    mapping(address=>address) public s_Token_To_Exchange;
+    mapping(address=>address) public s_Exchange_To_Token;
+
+    event exchange_Created(address token,address exchange);
 
     function createExchange(address token) public returns(address)
     {
+        require(token != address(0));
         require(s_Token_To_Exchange[token] == address(0));
         
         address new_exchange = address(new Exchange());
@@ -17,6 +20,18 @@ contract Factory
         s_Token_To_Exchange[token] = new_exchange;
         s_Exchange_To_Token[new_exchange] = token; 
 
+        emit exchange_Created(token, new_exchange);
+
         return new_exchange; 
+    }
+
+    function get_Exchange(address token) public view returns(address)
+    {
+        return s_Token_To_Exchange[token];
+    }
+
+    function get_Token(address exchange) public view returns(address)
+    {
+        return s_Exchange_To_Token[exchange];
     }
 }
---
+++ test/factory.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Factory} from "../src/factory.sol"; 
+
+contract FactoryTest is Test 
+{
+    Factory public factory; 
+
+    function setUp() public
+    {
+        factory = new Factory();
+    }
+
+    function test_Factory() public
+    {
+        address token = 0xf19681998788b7d1235cFd47C74510a51902A91B;
+        address newExchange = factory.createExchange(token);
+        console2.log("New Exchange:%s",newExchange);
+        assertNotEq(newExchange, address(0));
+        assertEq(token, factory.get_Token(newExchange));
+        assertEq(newExchange, factory.get_Exchange(token));
+    }
+
+}
