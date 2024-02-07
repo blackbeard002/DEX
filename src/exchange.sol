af09f77f9b0d6c3cbba56f4d54b9e8cd91e440df
Deepu <deepuharry02@gmail.com>
2024-02-07 23:41:46 +0530

    deleted interface directory

--- src/exchange.sol
+++ src/exchange.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.20;
 
-import "./interface/IERC20.sol";
+import "node_modules/@openzeppelin/contracts/interfaces/IERC20.sol";
 
 contract Exchange 
 {
